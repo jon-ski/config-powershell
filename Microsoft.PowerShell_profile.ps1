3371181f81f928e51fa3900e1e9e6b625920d80e
jon-ski <jonathan.isdna@gmail.com>
2025-07-30 15:00:28 -0600

    added htmlcopy and zoxide configuration

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -162,6 +162,39 @@ function tail {
     Get-Content $Path -Tail $n -Wait:$f
 }
 
+function htmlcopy {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [string] $InputObject
+    )
+
+    begin {
+        $sb = [System.Text.StringBuilder]::new()
+    }
+
+    process {
+        $null = $sb.AppendLine($InputObject)
+    }
+
+    end {
+        $data = $sb.ToString().TrimEnd()
+
+        if (-not $data) {
+            Write-Error "No input received"
+            return
+        }
+
+        Add-Type -AssemblyName System.Windows.Forms
+        $clip = New-Object Windows.Forms.DataObject
+        $clip.SetData("HTML Format", $data)
+        [Windows.Forms.Clipboard]::SetDataObject($clip, $true)
+    }
+}
+
+# Zoxide Setup
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
 # ==============================================================================
 # Help/Summary
 # ==============================================================================
